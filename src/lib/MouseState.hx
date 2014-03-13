55a72a847e5cf92a952518c731647d5c2dbc8e00
Nico <nico.may99@gmail.com>
2014-03-12 18:18:51 -0700

    Readded missing mouse input code, from FishSlice

---
+++ src/lib/MouseState.hx
@@ -0,0 +1,15 @@
+package lib;
+import flash.geom.Point;
+
+
+class MouseState
+{
+	public var position : Point;
+	public var leftButton : Bool = false;
+
+	public function new() 
+	{
+		position = new Point();
+	}
+	
+}
